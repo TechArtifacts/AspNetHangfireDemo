5d47ee6e5ce41e4648e94c6fa47505adddfa65f0
BHUSHAN ARUN KADAM <bhushan.kadam2709@gmail.com>
2022-12-22 00:05:42 +0530

    Added the demo project

---
+++ IHangfireTestJobService.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetHangfireDemo
+{
+    public interface IHangfireTestJobService
+    {
+        void AddFireAndForgetJob();
+        void AddReccuringJob();
+        void AddDelayedJob();
+        void AddContinuationJob();
+    }
+}
